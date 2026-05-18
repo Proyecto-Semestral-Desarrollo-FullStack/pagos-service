69b3b07a2247b340df53f9c8524880452addcf70
givenegas <gi.venegas@duocuc.cl>
2026-05-18 02:28:54 -0400

    Se integra Eureka Service

--- src/main/java/com/veterinaria/pagos/PagosServiceApplication.java
+++ src/main/java/com/veterinaria/pagos/PagosServiceApplication.java
@@ -2,8 +2,10 @@ package com.veterinaria.pagos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PagosServiceApplication {
 
 	public static void main(String[] args) {
